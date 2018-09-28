b3c6ce874d7295b82dc2cf77d3347ef78c576dda
Ushio <jylzs369@163.com>
2018-09-28 16:51:55 +0800

    初始化项目

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { takePicture, requestPermissions } from 'nativescript-camera';
+import { ImageAsset } from 'tns-core-modules/image-asset';
+
+@Component({
+    selector: "Home",
+    moduleId: module.id,
+    templateUrl: "./home.component.html",
+    styleUrls: ['./home.component.css']
+})
+
+export class HomeComponent {
+
+    constructor() { }
+
+
+    public saveToGallery: boolean = true;
+    public cameraImage: ImageAsset;
+
+    onTakePictureTap(args) {
+        requestPermissions().then(
+            () => {
+                takePicture({ width: 250, height: 300, keepAspectRatio: true, saveToGallery: this.saveToGallery })
+                    .then((imageAsset: any) => {
+                        this.cameraImage = imageAsset;
+                        imageAsset.getImageAsync(function (nativeImage) {
+                            let scale = 1;
+                            let height = 0;
+                            let width = 0;
+                            if (imageAsset.android) {
+                                // get the current density of the screen (dpi) and divide it by the default one to get the scale
+                                scale = nativeImage.getDensity() / imageAsset.android.util.DisplayMetrics.DENSITY_DEFAULT;
+                                height = imageAsset.options.height;
+                                width = imageAsset.options.width;
+                            } else {
+                                scale = nativeImage.scale;
+                                width = nativeImage.size.width * scale;
+                                height = nativeImage.size.height * scale;
+                            }
+                            console.log(`Displayed Size: ${width}x${height} with scale ${scale}`);
+                            console.log(`Image Size: ${width / scale}x${height / scale}`);
+                        });
+                    }, (error) => {
+                        console.log("Error: " + error);
+                    });
+            },
+            () => alert('permissions rejected')
+        );
+    }
+}
